06b92a5e8f3239dd8d2a3628fdb971c07cba7949
Subrato Mondal <subratomondal374@gmail.com>
2024-09-23 21:53:52 +0530

    learning js

---
+++ HigherOrderFucn.js
@@ -0,0 +1,14 @@
+// higher order function
+// aisa koi function jo ki ek function ko parameter mein accept karle and/or ek function ko return karde
+
+// example
+function abcd(pass){
+
+}
+abcd(function(){});
+
+function abcd(){
+    return function(){
+
+    }
+}
